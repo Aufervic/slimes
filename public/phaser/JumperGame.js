7904d59aa91b78a2c0335a07cf3f1eff8b411f12
Aufer Victoriano <aufer.vicc@gmail.com>
2023-06-18 00:29:59 -0500

    Se eliminaron los archivos de deploy de otros servicios

--- public/phaser/JumperGame.js
+++ public/phaser/JumperGame.js
@@ -182,7 +182,6 @@ class JumperScene extends Phaser.Scene {
   defineSocket() {
     const self = this
     this.socket = io();
-    // this.socket = io();
     this.socket.on("currentData", ({players, bombs}) => {
       Object.keys(players).forEach((id) =>{
         if (players[id].playerId === self.socket.id) {
